5980cfbe6eac1c0a13a9e30a42b9d535bd54922c
SteffLD50 <sanjacizik@gmail.com>
2024-02-05 20:40:50 +0100

    fakers

---
+++ cypress/e2e/createGalleryFaker.cy.js
@@ -0,0 +1,30 @@
+/// <reference types='Cypress'/>
+import { faker } from "@faker-js/faker";
+
+let companyName = faker.company.name();
+
+describe("Login and create Gallery - Gallery App", () => {
+  before(() => {});
+
+  beforeEach(() => {
+    cy.visit("/login");
+
+    cy.get("#email").type("peraperic12345@gmail.com");
+    cy.get("#password").type("pera12345");
+    cy.get("button").click();
+  });
+
+  it("Successfully login and open new Gallery page", () => {
+    cy.url().should("not.include", "/login");
+
+    cy.visit("/create");
+
+    cy.get("#title").type(companyName);
+    cy.get("#description").type("Brownie Gallery description");
+    cy.get("[type='url']").type(
+      "https://www.simplyrecipes.com/thmb/w52xKYdM2peskIGtntk3i4202sU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SimplyRecipes_CosmicBrownies_LEAD_5-01ab51e01a644e87becbdec1a7a3b67c.jpg"
+    );
+
+    cy.get(".btn-custom").contains("Submit").click();
+  });
+});
